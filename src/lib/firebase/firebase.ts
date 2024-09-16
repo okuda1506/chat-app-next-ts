b1b5299ffa33ecf8ece9bd70eedc1ace8cfc3c49
okuda1506 <okuda1506@gmail.com>
2024-09-16 13:13:30 +0900

    エラーの修正

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {
     FIREBASE_API_KEY,
     FIREBASE_APP_ID,
