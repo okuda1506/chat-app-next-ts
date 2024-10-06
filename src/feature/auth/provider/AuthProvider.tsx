529ca585e5052eec5319508d86606d90a1cff92e
okuda1506 <okuda1506@gmail.com>
2024-10-06 17:27:13 +0900

    AuthProviderへのプロフィール画像URL保存の実装

--- src/feature/auth/provider/AuthProvider.tsx
+++ src/feature/auth/provider/AuthProvider.tsx
@@ -7,36 +7,49 @@ import {
 } from 'react'
 import type { User } from '@firebase/auth'
 import { getAuth, onAuthStateChanged } from '@firebase/auth'
+import { storage } from "@src/lib/firebase/firebase"
+import { getDownloadURL, ref} from "firebase/storage"
 
 export type GlobalAuthState = {
-user: User | null | undefined
+    user: User | null | undefined
+    profileImageUrl: string | null
 }
 const initialState: GlobalAuthState = {
-user: undefined,
+    user: undefined,
+    profileImageUrl: null
 }
 const AuthContext = createContext<GlobalAuthState>(initialState)
 
 type Props = { children: ReactNode }
 
 export const AuthProvider = ({ children }: Props) => {
-const [user, setUser] = useState<GlobalAuthState>(initialState)
+    // const [user, setUser] = useState<GlobalAuthState>(initialState)
+    const [authState, setAuthState] = useState<GlobalAuthState>(initialState)
 
 useEffect(() => {
     try {
-    const auth = getAuth()
-    return onAuthStateChanged(auth, (user) => {
-        setUser({
-        user,
+        const auth = getAuth()
+        return onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                const imageRef = ref(storage, `images/${user.uid}`)
+                const profileImageUrl = await getDownloadURL(imageRef)
+
+                setAuthState({
+                    user, 
+                    profileImageUrl,
+                })
+            } else {
+                setAuthState(initialState)
+            }
         })
-    })
     } catch (error) {
-    setUser(initialState)
-    throw error
+        setAuthState(initialState)
+        throw error
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
 }, [])
 
-return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>
+return <AuthContext.Provider value={authState}>{children}</AuthContext.Provider>
 }
 
 export const useAuthContext = () => useContext(AuthContext)
